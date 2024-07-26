f9a38325052633778e155460364db3b16e2c1712
Diogo H. <88913575+diogo-has@users.noreply.github.com>
2024-07-26 08:55:00 -0300

    Add files via upload

---
+++ ATV02/Empresa2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Empresa2
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public void ExibirInfo()
+        {
+            Console.WriteLine($"Nome: {Nome}\nEmail: {Email}");
+        }
+    }
+
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto(string nome, float preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public void ExibirInfo()
+        {
+            Console.WriteLine($"Nome: {Nome}\nPreço: {Preco}");
+        }
+    }
+
+    public class Carrinho
+    {
+        public Cliente Cliente { get; set; }
+        public List<Produto> Produtos { get; set; }
+
+        public Carrinho(Cliente cliente, List<Produto> produtos)
+        {
+            Cliente = cliente;
+            Produtos = produtos;
+        }
+    }
+}
---
+++ ATV02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Empresa2;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cliente joao = new Cliente("João", "joao@example.com");
+            Produto suco = new Produto("Suco de uva", 12.9f);
+            Produto macarrao = new Produto("Macarrão espaguete", 6.7f);
+
+            List<Produto> produtos = new List<Produto>();
+
+            produtos.Add(suco);
+            produtos.Add(macarrao);
+
+            Carrinho carrinho1 = new Carrinho(joao, produtos);
+
+            foreach (Produto a in carrinho1.Produtos)
+            {
+                Console.WriteLine($"Nome: {a.Nome}\nPreço: {a.Preco}");
+            }
+        }
+    }
+}
